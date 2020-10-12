21b261666d462e041ff9b81595b07f7c7c103045
“cuixinyu” <524000363@qq.com>
2020-10-12 16:15:34 +0800

    Site updated: 2020-10-12 16:15:27

--- js/cuixinyu.js
+++ js/cuixinyu.js
@@ -1,4 +1,5 @@
 $(function(){
     var xiaokang = new xkTool();
     xiaokang.footFish();
+    xiaokang.cheatTitle(["！！这里这里 ◕ ں ◕"], ["(ฅ>ω<*ฅ) 欢迎回来哦！爱你哟~"], ["https://cdn.jsdelivr.net/gh/cuixinyu0427/cdn/img/avatar.png"], ["https://cdn.jsdelivr.net/gh/cuixinyu0427/cdn/img/avatar.png"]);
 })
