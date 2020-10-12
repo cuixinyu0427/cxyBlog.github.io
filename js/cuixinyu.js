df56ba0fc4e9d53eb4d8b3a63c2aa5b18331e46e
cuixinyu980427 <524000363@qq.com>
2020-10-12 21:03:50 +0800

    Site updated: 2020-10-12 21:03:46

--- js/cuixinyu.js
+++ js/cuixinyu.js
@@ -2,4 +2,5 @@ $(function(){
     var xiaokang = new xkTool();
     xiaokang.footFish();
     xiaokang.cheatTitle(["看不见我🙈~看不见我🙈~"], ["(°ー°〃) 被发现了~"], ["https://cdn.jsdelivr.net/gh/cuixinyu0427/cdn/img/avatar.png"], ["https://cdn.jsdelivr.net/gh/cuixinyu0427/cdn/img/avatar.png"]);
+    xiaokang.magicCirle([18], [0.1], ["white"], [0.3]);
 })
