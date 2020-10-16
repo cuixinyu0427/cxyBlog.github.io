209bb19ce1c361ddd5921f262bc132aeb57f590e
“cuixinyu” <524000363@qq.com>
2020-10-16 16:28:25 +0800

    Site updated: 2020-10-16 16:28:21

--- js/cuixinyu.js
+++ js/cuixinyu.js
@@ -54,53 +54,85 @@ $(function(){
 
     xiaokang.cheatTitle(["看不见我🙈~看不见我🙈~"], ["(°ー°〃) 被发现了~"], ["https://cdn.jsdelivr.net/gh/cuixinyu0427/cdn/img/avatar.png"], ["https://cdn.jsdelivr.net/gh/cuixinyu0427/cdn/img/avatar.png"]);
 
-    var loadingMsgIndex,
-        botui = new BotUI('stars-bot'),
-        API = 'https://api.github.com/repos/';
+    var botui = BotUI('home-demo');
 
-    function sendXHR(repo, cb) {
-        var xhr = new XMLHttpRequest();
-        var self = this;
-        xhr.open('GET', API + repo);
-        xhr.onload = function () {
-            var res = JSON.parse(xhr.responseText)
-            cb(res.stargazers_count);
-        }
-        xhr.send();
-    }
-
-    function init() {
-        botui.message
-            .bot({
-                delay: 1000,
-                content: 'Enter the repo name to see how many stars it have:'
-            })
-            .then(function () {
-                return botui.action.text({
-                    delay: 1000,
-                    action: {
-                        value: 'moinism/botui',
-                        placeholder: 'moinism/botui'
-                    }
-                })
-            }).then(function (res) {
-            loadingMsgIndex = botui.message.bot({
-                delay: 200,
-                loading: true
-            }).then(function (index) {
-                loadingMsgIndex = index;
-                sendXHR(res.value, showStars)
-            });
+    botui.message.add({
+        content: '大家好，我是千颂伊！'
+    }).then(function () {
+        return botui.message.add({
+            delay: 1500,
+            content: '![千颂伊](http://img3.duitang.com/uploads/blog/201402/15/20140215120608_BzmRN.gif)'
         });
-    }
+    }).then(function () {
+        return botui.action.button({
+            delay: 1000,
+            action: [{
+                text: '继续聊 💕',
+                value: 'sure'
+            }, {
+                text: '下回聊 💔',
+                value: 'skip'
+            }]
+        });
+    }).then(function (res) {
+        if(res.value == 'sure') {
+            tutorial();
+        }
+        if(res.value == 'skip') {
+            end();
+        }
+    });
 
-    function showStars(stars) {
-        botui.message
-            .update(loadingMsgIndex, {
-                content: 'it has !(star) ' + (stars || "0") + ' stars.'
-            })
-            .then(init); // ask again for repo. Keep in loop.
-    }
+    var tutorial = function () {
+        botui.message.add({
+            delay: 1000,
+            content: "太棒了，让我们简单互动一下吧。"
+        }).then(function () {
+            return botui.message.add({
+                delay: 1000,
+                content: '请问我男票是谁？（输入名字后按回车键！）'
+            });
+        }).then(function () {
+            return botui.action.text({
+                delay: 800,
+                action: {
+                    value: '都敏俊',
+                    placeholder: '千颂伊的男朋友'
+                }
+            });
+        }).then(function (res) {
+            return botui.message.bot({
+                delay: 500,
+                content: '没错，就是' + res.value + '！'
+            });
+        }).then(function (res) {
+            return botui.message.bot({
+                delay: 1400,
+                content: '별에서 온 그대 ![合影](http://www.hanyouwang.com/uploads/2014/0303/20140303032047599.jpg)'
+            });
+        }).then(function (res) {
+            return botui.message.add({
+                delay: 1200,
+                type: 'embed',
+                content: 'http://player.youku.com/embed/XNjc4ODY5NzY4'
+            });
+        }).then(function (res) {
+            return botui.message.bot({
+                delay: 2500,
+                content: '祝天下有情人终成眷属！'
+            });
+        }).then(function (res) {
+            return botui.message.bot({
+                delay: 3888,
+                content: '我为BotUI带盐（注意：vue.js为2.0.5）'
+            });
+        }).then(end);
+    };
 
-    init();
+    var end = function () {
+        botui.message.add({
+            delay: 3888,
+            content: '!(book) [帮助文档](https:///docs.botui.org)，查看 [示例](https:///examples.botui.org) 或 [点击浏览代码](https://github.com/moinism/botui)'
+        });
+    };
 })
